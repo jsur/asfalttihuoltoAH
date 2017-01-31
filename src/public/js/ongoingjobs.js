db2253b3a54312bf5131d26e155db961d9df71ca
Julius Suominen <julius.suominen@gmail.com>
2017-01-31 22:05:11 +0200

    ongoingjobs refactoring

--- src/public/js/ongoingjobs.js
+++ src/public/js/ongoingjobs.js
@@ -4,6 +4,11 @@ $(function() {
 
 	$("tbody tr").click(function() {
 
+		var id = this.id;
+		var i = $(this).index();
+
+		giveJobStatusButtonStyles(id, i);
+		console.log(id, i);
 
 		if($(this).hasClass('selected')) {
 			$(this).removeClass('selected');
@@ -11,50 +16,89 @@ $(function() {
 		} else {
 			$("tbody tr").not(this).removeClass('selected');
 			$(this).toggleClass('selected');
-			$(".job-start").removeAttr('disabled');
 			$(".container-fluid").addClass('container-fluid-row-edit');
-			if($(".job-statusgroup-container").attr("hidden")) {
-				$(".job-statusgroup-container").slideToggle("fast").removeAttr("hidden");
 			}
+
+		if($(".job-statusgroup-container").attr("hidden")) {
+			$(".job-statusgroup-container").slideToggle('fast').removeAttr('hidden');
+		} else if(!$("tbody tr").hasClass('selected')) {
+			$(".job-statusgroup-container").slideToggle('fast').attr('hidden', 'true');
 		}
 	});
 
 	$(".job-start").click(function() {
 
 		if($(".job-start").hasClass("btn-success")) {
-			$(".job-start").
-				removeClass("btn-success").
-				html("Aloita työ").
-				append("<span class='glyphicon glyphicon-play'></span>");
-			$(".job-ready").attr('disabled', 'true');
+			removeJobStatusStarted();
 		} else {
-			$(".job-start").
-				toggleClass("btn-success").
-				html("Aloitettu").
-				append("<span class='glyphicon glyphicon-ok'></span>");
-			$(".job-ready").removeAttr('disabled');
+			setJobStatusStarted();
 		}
 	});
 
 	$(".job-ready").click(function() {
 
 		if($(".job-ready").hasClass("btn-success")) {
-			$(".job-ready").
-				removeClass("btn-success").
-				html("Aseta valmiiksi").
-				append("<span class='glyphicon glyphicon-flag'></span>");
-			$(".job-billed").attr('disabled', 'true');
+			removeJobStatusReady();
 		} else {
-			$(".job-ready").
-				toggleClass("btn-success").
-				html("Työ valmis!").
-				append("<span class='glyphicon glyphicon-ok'></span>");
-			$(".job-billed").removeAttr('disabled');
+			setJobStatusReady();
 		}
 	});
 
+/*Functions for events*/
+
+	function giveJobStatusButtonStyles(id, i) {
+		
+		if(data[i].id == id && data[i].started == false && data[i].completed == false) {
+			//If only final billing is needed
+			$(".job-ready").removeAttr('disabled');
+			setJobStatusStarted();
+			setJobStatusReady();
+
+		} else if (data[i].id == id && data[i].started == true && data[i].completed == false) {
+			//If job has only been started
+			setJobStatusStarted();
+		} else {
+			$(".job-start").removeAttr('disabled');
+			console.log('meni tänne');
+		}
+	};
+
+	function setJobStatusStarted() {
+		$(".job-start").
+			toggleClass("btn-success").
+			html("Aloitettu").
+			append("<span class='glyphicon glyphicon-ok'></span>");
+		$(".job-ready").removeAttr('disabled');
+	}
+
+	function removeJobStatusStarted() {
+		$(".job-start").
+			removeClass("btn-success").
+			html("Aloita työ").
+			append("<span class='glyphicon glyphicon-play'></span>");
+		$(".job-ready").attr('disabled', 'true');
+	}
+
+	function setJobStatusReady() {
+		$(".job-ready").
+			toggleClass("btn-success").
+			html("Työ valmis!").
+			append("<span class='glyphicon glyphicon-ok'></span>");
+		$(".job-billed").removeAttr('disabled');
+		$(".job-start").attr('disabled', 'true');
+	}
+
+	function removeJobStatusReady() {
+		$(".job-ready").
+			removeClass("btn-success").
+			html("Aseta valmiiksi").
+			append("<span class='glyphicon glyphicon-flag'></span>");
+		$(".job-billed").attr('disabled', 'true');
+		$(".job-start").removeAttr('disabled');
+	}
 
 });
 
 
 /*AJAX calls for button actions*/
+
